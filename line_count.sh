3c7ada9b4e9e911ce50b8c21d7015f50a0129e37
Rafał Grodziński <rafal.grodzinski@gmail.com>
2013-03-12 16:58:19 +0100

    Now we count lines better

--- line_count.sh
+++ line_count.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -name "*.[hm]" -print0 | xargs -0 wc -l
+find . \( ! -path ./Rapid\ Note\ Go/Source/Utils/APXML/\* \) -a \( ! -path ./Rapid\ Note\ Go/Source/Utils/IXPickerOverlayView/\* \) -a \( ! -path ./Rapid\ Note\ Go/Source/Utils/KSCustomPopoverBackgroundView/\* \) -a \( ! -path ./Rapid\ Note\ Go/Source/Utils/LTKPopoverActionSheet/\* \) -name "*.[hm]" -print0 | xargs -0 wc -l
